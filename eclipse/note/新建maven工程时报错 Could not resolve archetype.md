5b1fd615d489e00c92e240e4d2046606ccf09d93
wujianhui <wjh_q@163.com>
2018-10-08 19:52:00 +0800

    新增eclipese工具搭建springboot项目demo

---
+++ eclipse/note/新建maven工程时报错 Could not resolve archetype.md
@@ -0,0 +1,11 @@
+﻿# 新建maven工程时报错 Could not resolve archetype
+
+标签（空格分隔）： 编程资料 开发工具 eclipse
+###### create date 2018-10-08 17:09:44
+---
+
+## 解决思路
+    1.下载最新版maven-archetype-quickstart-1.1.jar
+        http://www.java2s.com/Code/Jar/m/Downloadmavenarchetypequickstart11jar.htm
+    2.cmd窗口cd到maven-archetype-quickstart-1.1.jar目录
+    3.执行mvn install:install-file -DgroupId=org.apache.maven.archetypes -DartifactId=maven-archetype-quickstart -Dversion=1.1 -Dpackaging=jar -Dfile=maven-archetype-quickstart-1.1.jar
---
+++ eclipse/test/spring-boot-ceshi/spring-boot-ceshi-basic-logic/src/main/java/org/spring/boot/ceshi/basic/logic/Application.java
@@ -0,0 +1,41 @@
+package org.spring.boot.ceshi.basic.logic;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
+/**
+ * Spring Boot 启动类
+ * @author w
+ */
+@Log4j
+@RestController
+@SpringBootApplication
+@ComponentScan(value = "com.spring.boot.test.*")
+@EnableAutoConfiguration
+@ServletComponentScan
+//@EnableTransactionManagement
+public class Application {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String defaultPage(HttpSession session) {
+        session.setAttribute("uid", UUID.randomUUID());
+
+        return session.getAttribute("uid").toString();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("user.dir:"+System.getProperty("user.dir"));
+    }
+}
